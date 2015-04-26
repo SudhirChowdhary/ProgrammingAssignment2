34a8dc01212586c84738d6529eddcf71ead3bca7
Sudhir <sudhirchowdhary@gmail.com>
2015-04-26 00:02:33 -0500

    Code Completed for Assignment 2
    Code Completed for Assignment 2 (some Comments still remaining)

--- cachematrix.R
+++ cachematrix.R
@@ -1,15 +1,42 @@
-## Put comments here that give an overall description of what your
-## functions do
+## Matrix inversion is usually a costly computation and there may be some benefit to caching the inverse of a
+## matrix rather than compute it repeatedly (there are also alternatives to matrix inversion that we will not discuss here).
+## Your assignment is to write a pair of functions that cache the inverse of a matrix.
+
 
 ## makeCacheMatrix: This function creates a special "matrix" object that can cache its inverse.
+## This Function creates a list containing with following functionality
+## a. Set Value of Matrix.
+## b. Get Value of Matrix.
+## c. Set Value of Inverse Matrix.
+## d. Get Value of Inverse Matrix.
+
 
 makeCacheMatrix <- function(x = matrix()) {
-
+        ## <<- Operator cause a search to made through parent environments for an existing definition of the variable being assigned
+        inv <- NULL ## Initialize the Values to NULL
+        set <- function(y) {
+                x <<- y  ## Cache the matrix
+                inv <<- NULL  ## set the value of inv to NULL
+        }
+        get <- function() x
+        setinverse <- function(inverse) inv <<- inverse
+        getinverse <- function() inv
+        list(set=set, get=get, setinverse=setinverse, getinverse=getinverse)
 }
 
-
-## Write a short comment describing this function
+## This function computes the inverse of the special "matrix" returned by makeCacheMatrix above. If the inverse has already been calculated (and the matrix has not changed), then the cachesolve should retrieve the inverse from the cache.
 
 cacheSolve <- function(x, ...) {
         ## Return a matrix that is the inverse of 'x'
+        inv <- x$getinverse()
+        ## For the first time, inv will have NULL value. Second time, the value will be <> NULL
+        if(!is.null(inv)) {
+                message("Cached Data")
+                return(inv)
+        }
+        data <- x$get()
+        inv <- solve(data)
+        ## Sets the value of inv to <> NULL, so that we get the value from other environment.
+        x$setinverse(inv)
+        inv
 }
